c516681fd9c964da551e56f2bd7036f61379dc20
Ghost-Element <danieldw@edu.aau.at>
2026-01-06 15:34:40 +0100

    all code newly added

---
+++ app/models/geocode.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class SimpleLocation(BaseModel):
+    name: str
+    lat: float
+    lon: float
+
+class Location(BaseModel):
+    full_name: str
+    lat: float
+    lon: float
+    house_number: str | None
+
+class LocationList(BaseModel):
+    locations: list[Location]
+
+class LocationRequest(BaseModel):
+    street: str
+    houseNumber: str
+    city: str
+    postalCode: str
+    country: Optional[str] = None
+
+class UserLocation(BaseModel):
+    username: str
+    location: SimpleLocation
+
+class User(BaseModel):
+    username: str
+
